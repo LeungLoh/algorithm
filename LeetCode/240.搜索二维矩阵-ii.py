bfaf1574d887b9a1e686d736b40bb48a62ff255b
LeungLoh <843314152@qq.com>
2022-08-23 13:01:55 +0800

    update

---
+++ LeetCode/240.搜索二维矩阵-ii.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode.cn id=240 lang=python3
+#
+# [240] 搜索二维矩阵 II
+#
+
+# @lc code=start
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        i, j = 0, len(matrix[0]) - 1
+        while i < len(matrix) and j >= 0:
+            if matrix[i][j] == target:
+                return True
+            if matrix[i][j] > target:
+                j -= 1
+            else:
+                i += 1
+        return False
+
+
+# @lc code=end
